96e979ddd1aafdcf1b3332e64096c7ef4954c0cb
SENA <SENA@ARMSCCCCTGOD211.FormacionSena.RED>
2025-02-28 11:15:44 -0500

    avances de clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Greet } from "./assets/components/Greet"
+import { Home } from "./assets/components/Pages/Home/Home.jsx"
 
 export const App = () =>{
   return(
     <>
-    <Greet />
+      <Home/>
     </>
   )
 }
---
+++ src/assets/components/Layouts/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ItemNavbar } from '../../Ui/ItemNavbar/ItemNavbar'
+
+export const Navbar = () => {
+  return (
+    <>
+        <nav>
+            <ItemNavbar />
+        </nav>
+    </>
+  )
+}
